bdc6d1e89cea0c7b74797c0edc1378907c0ea378
adiodeji <106891740+adiodeji@users.noreply.github.com>
2022-06-12 21:03:49 +0000

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+/*Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+-Boyer-Moore Voting Algorithm*/
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int count =0;
+        int candidate=0;
+        for(int num: nums){
+            if(count==0){
+                candidate=num;
+            }
+            if(num==candidate)count++;
+            else count--;
+        }
+        
+        return candidate;
+        
+    }
+}
